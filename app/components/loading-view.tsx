fc2c1ebc1c9b49920c2a6d781d59a154a66c6398
Rafal Pastuszak <rafal.pastuszak@gmail.com>
2023-07-01 19:59:57 +0100

    chore: better mobile layout

--- app/components/loading-view.tsx
+++ app/components/loading-view.tsx
@@ -1,9 +1,10 @@
 import { FC } from "react";
 import { Button } from "./button";
+import styles from "./loading-view.module.css";
 
 export const LoadingView: FC = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <h2>OK, let me think...</h2>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -11,19 +11,21 @@ import { Logo } from "./components/logo";
 export default function Home() {
   const { status } = useAppStore();
   return (
-    <div className={styles.pageContainer}>
-      <header className={styles.header}>
-        <Logo />
-      </header>
-      <main className={styles.main}>
-        {status === "idle" ? <InputView /> : null}
-        {status === "loading" ? <LoadingView /> : null}
-        {status === "results" ? <ResultsView /> : null}
-        {status === "error" ? <ErrorView /> : null}
-      </main>
-      <footer className={styles.footer}>
-        Made by 🐐 and <a href="https://sonnet.io">Rafal</a>.
-      </footer>
+    <div className={styles.viewportContainer}>
+      <div className={styles.pageContainer}>
+        <header className={styles.header}>
+          <Logo />
+        </header>
+        <main className={styles.main}>
+          {status === "idle" ? <InputView /> : null}
+          {status === "loading" ? <LoadingView /> : null}
+          {status === "results" ? <ResultsView /> : null}
+          {status === "error" ? <ErrorView /> : null}
+        </main>
+        <footer className={styles.footer}>
+          Made by 🐐 and <a href="https://sonnet.io">Rafal</a>.
+        </footer>
+      </div>
     </div>
   );
 }
