f18b2627b3b633b4def882b76170beef690e1dfe
Rafal Pastuszak <rafal.pastuszak@gmail.com>
2023-07-03 12:35:40 +0100

    chore: use the Vercel-blessed way of updating OG meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,30 @@
+import { Metadata } from "next";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Tip of the Tongue",
-  description: "",
+  description:
+    "Has your parahippocampal gyrus activation decreased? We can help!",
+  authors: [
+    {
+      name: "Rafał Pastuszak",
+    },
+  ],
+  twitter: {
+    card: "summary_large_image",
+    creator: "@rafalpast",
+  },
+  openGraph: {
+    title: "Tip of the Tongue",
+    description:
+      "Has your parahippocampal gyrus activation decreased? We can help!",
+    type: "website",
+    url: "https://tip.potato.horse",
+    siteName: "Tip of the Tongue",
+  },
 };
 
 export default function RootLayout({
