6b35787e67f89bd653e548d67a922780672a757b
Rafal Pastuszak <rafal.pastuszak@gmail.com>
2023-07-05 14:31:50 +0100

    chore: log questions

---
+++ check-guesses.mjs
@@ -0,0 +1,27 @@
+import { kv } from "@vercel/kv";
+import { response } from "msw";
+
+const run = async () => {
+  const [_, keys] = await kv.scan(0, {
+    match: "description:*",
+  });
+
+  const events = await kv.mget(...keys);
+
+  const formattedEvents = events
+    .filter((event) => event?.request && event?.response)
+    .map(({ request, response }) =>
+      `
+    
+Q: 
+${request.description}
+
+A: 
+ - ${(response.guesses.map((_) => _.label) || []).join("\n - ")}
+
+    `.trim()
+    );
+
+  console.log(formattedEvents.join("\n\n"));
+};
+run();
