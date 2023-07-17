7d46f29ec8704af4ad46493cd0db8ce54b893a2d
Rafal Pastuszak <rafal.pastuszak@gmail.com>
2023-07-17 18:44:43 +0100

    fix: wait for the kv response to finish before return

--- check-guesses.mjs
+++ check-guesses.mjs
@@ -3,9 +3,15 @@ import { kv } from "@vercel/kv";
 const run = async () => {
   const [_, keys] = await kv.scan(0, {
     match: "description:*",
+    count: 1000,
   });
 
+  console.log(`Found ${keys.length} keys`);
+
   const events = await kv.mget(...keys);
+
+  console.log(`Found ${events.length} events`);
+  console.log({ events });
 
   const formattedEvents = events
     .filter((event) => event?.request && event?.response)
